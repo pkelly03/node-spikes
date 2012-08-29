d1cde31bcfb03bed18747101ce07fbe6659a78ab
Paul Kelly <pkelly@equalexperts.com>
2012-08-29 11:40:55 +0100

    Initial add for node spikes

---
+++ chat.js
@@ -0,0 +1,14 @@
+var net = require('net');
+var chatServer = net.createServer(),
+	clientList = [];
+chatServer.on('connection', function(client) {
+	client.write('Hi!\n');
+	clientList.push(client);
+
+	client.on('data', function(data) {
+		for (var i = 0; i < clientList.length; i += 1) {
+			clientList[i].write(data);
+		}
+	})
+})
+chatServer.listen(9000);
